b83f2928921c7c745b36ebc597339e9c6f09117c
nisar <nisar2048@gmail.com>
2018-06-03 13:55:10 +0530

    invita bot ui

--- bot/invita/botchat-override.js
+++ bot/invita/botchat-override.js
@@ -2,33 +2,41 @@ var botElement = document.querySelector('#bot');
 var arrow = document.querySelector('.arrow');
 var close = document.querySelector('.close');
 var open = document.querySelector('.open');
+var poweredby = document.querySelector('.poweredby');
 
 botElement.style.opacity = 0;
 close.style.display = "none";
+poweredby.style.display = "none";
 showBot = function () {
     if (botElement.style.opacity == 0) {
         botElement.style.opacity = 1;
         arrow.style.display = "none";
+        poweredby.style.display = "block";
         close.style.display = "block";
     } else {
         botElement.style.opacity = 0;
         arrow.style.display = "block";
+        poweredby.style.display = "none";
         close.style.display = "none";
     }
 };
 
 maximizeBot = function () {
     var panel = document.querySelector('.wc-chatview-panel');
-    if (close.style.bottom != "615px") {
+    var poweredby = document.querySelector('.poweredby');
+    var expanded_close_bottom = "630px";
+    if (close.style.bottom != expanded_close_bottom) {
         panel.style.width = '500px';
         panel.style.height = '650px';
-        close.style.bottom = '615px';
-        open.style.bottom = '615px';
+        close.style.bottom = expanded_close_bottom;
+        open.style.bottom = expanded_close_bottom;
+        poweredby.style.width = '484px';
     }else{
         panel.style.width = '350px';
         panel.style.height = '500px';
-        close.style.bottom = '465px';
-        open.style.bottom = '465px';
+        close.style.bottom = '480px';
+        open.style.bottom = '480px';
+        poweredby.style.width = '334px';
     }
 
 }
